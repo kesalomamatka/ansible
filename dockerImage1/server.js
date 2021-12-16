9973b0e92ee2739feb2dff2b1dfc167dbe9dfa98
zhihao tan <zhihao.tan@tuni.fi>
2021-12-16 03:40:07 +0200

    Ansible exercise

---
+++ dockerImage1/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8081,()=>{
+    console.log("Listen on 8081")
+});
